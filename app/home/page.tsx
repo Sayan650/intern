38355d11cf7ff8f22272f3f90135c30557c638c3
Sayan650 <sayanp650@gmail.com>
2023-10-10 19:09:07 +0530

    main page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,12 @@
-import Landing from '@/components/landing/Landing'
-import Navbar from '@/components/navbar/Navbar'
+// import Landing from '@/components/landing/Landing'
+// import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        <Navbar/>
-        <Landing/>
+        {/* <Navbar/>
+        <Landing/> */}
     </div>
   )
 }
